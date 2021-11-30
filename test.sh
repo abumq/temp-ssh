2fdef358ec24f4b4ef01545d4f29b17062df3122
abumusamq <mkhan3189@gmail.com>
2021-11-30 14:02:40 +1100

    transfer to include --partial

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ IP=127.0.0.1
 PORT=2211
 
 touch test.file
-rsync -av --progress -e "ssh -p $PORT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ./test.file test@$IP:/data/
+rsync -avP --progress -e "ssh -p $PORT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" ./test.file test@$IP:/data/
