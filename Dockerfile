ae0b3a48b65feb05fc315f17eb6a315c587c9023
Majid Khan <majid.khan@domain.com.au>
2022-10-20 16:55:16 +1100

    Update to temp ssh

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ RUN useradd -rm -d /home/ubuntu -s /bin/bash -g root -G sudo -u 1000 test
 
 RUN service ssh start
 
+RUN echo 'root:root' | chpasswd
+RUN echo 'test:test' | chpasswd
+
 EXPOSE 22
 
 CMD ["/usr/sbin/sshd", "-D"]
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 PORT=2211
-docker run --rm -it -p $PORT:22 -v `pwd`/data:/data -v `pwd`/ssh/authorized_keys:/home/ubuntu/.ssh/authorized_keys abumusamq/temp-ssh
+docker run --rm -it --name tempssh -p $PORT:22 -v `pwd`/data:/data -v `pwd`/ssh/authorized_keys:/home/ubuntu/.ssh/authorized_keys abumusamq/temp-ssh
